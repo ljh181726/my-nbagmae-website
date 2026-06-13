b8f4bf99b139ed13e287a24e192d60ac333ff22e
林嵥恆 <ljh181726@gmail.com>
2026-06-13 23:07:00 +0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -97,14 +97,14 @@ app.get('/healthz', (req, res) => {
 
 app.get('/api/config', (req, res) => {
   res.json({
-    hasGeminiKey: !!process.env.GEMINI_API_KEY,
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    hasGeminiKey: !!(process.env.GEMINI_API_KEY),
+    apiKey: process.env.FIREBASE_API_KEY || "",
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN || "",
+    projectId: process.env.FIREBASE_PROJECT_ID || "",
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "",
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "",
+    appId: process.env.FIREBASE_APP_ID || "",
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID || ""
   });
 });
 
